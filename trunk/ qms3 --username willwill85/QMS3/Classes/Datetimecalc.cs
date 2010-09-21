5c067d0e247ed9f15c01b99e97019070206b02d8
willwill85 <willwill85@ac8fd80a-bfc5-91ec-f4f3-72c25b0952e3>
2010-09-21 12:53:24 +0000

    添加了几个tab 关于图表，修改了几处sliptpanel

---
+++ trunk/ qms3 --username willwill85/QMS3/Classes/Datetimecalc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QMS3.Classes
+{
+    class Datetimecalc
+    {
+        static public int daysofmonth(DateTime dt)
+        {
+            DateTime date = dt;
+            int tday = dt.Day;
+            tday--;
+            tday = tday * -1;
+            date=date.AddDays(tday);
+            int i;
+            for (i = 28; i <= 32; i++)
+            {
+                DateTime tempdate = date;  
+                if (tempdate.AddDays(i).Month > date.Month)
+                    return i;
+            }
+            return i;
+        }
+    }
+}
