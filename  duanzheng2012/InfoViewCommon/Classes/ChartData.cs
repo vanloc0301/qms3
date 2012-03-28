49c149f9064bd13d0a5dd8a3b8f4c0aab5f5cf46
duanzheng2012 <duanzheng2012@ac8fd80a-bfc5-91ec-f4f3-72c25b0952e3>
2012-03-28 03:41:09 +0000

    dz 2010.3.28 提取出公共模块 添加新的运输时间分布饼图

---
+++  duanzheng2012/InfoViewCommon/Classes/ChartData.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using Microsoft.Office.Interop;
+using Microsoft.Office.Core;
+using System.Threading;
+
+namespace InfoViewCommon.Classes
+{
+    public class ChartData
+    {
+        public DateTime yeardatetime;
+        public double[] year;
+        public double[] lastyear;
+
+        public double[] month;
+        public double[] lastmonth;
+        public double[] lastyearmonth;
+
+        public int stationID;
+        public DateTime monthdatetime;
+        public double[] stationyear;
+        public double[] stationlastyear;
+
+        public double[] stationmonth;
+        public double[] stationlastmonth;
+        public double[] stationlastyearmonth;
+
+        public int[] stationdaybox;
+
+        private int[] PercentOfThread;
+
+
+        public ChartData()
+        {
+            this.year = new double[12];
+            this.lastyear = new double[12];
+            this.stationyear = new double[12]; ;
+            this.stationlastyear = new double[12]; 
+           
+            for (int i = 0; i <= 11; i++)
+            {
+                this.year[i] = 0;
+                this.lastyear[i] = 0;
+                this.stationyear[i] = 0;
+                this.stationlastyear[i] = 0;
+            }
+            this.month = new double[31];
+            this.lastmonth = new double[31]; ;
+            this.lastyearmonth = new double[31];
+            this.stationmonth = new double[31];
+            this.stationlastmonth = new double[31];
+            this.stationlastyearmonth = new double[31]; 
+            for (int i = 0; i <= 30; i++)
+            {
+                this.month[i] = 0;
+                this.lastmonth[i] = 0;
+                this.lastyearmonth[i] = 0;
+                this.stationmonth[i] = 0;
+                this.stationlastmonth[i] = 0;
+                this.stationlastyearmonth[i] = 0;
+            }
+
+            this.PercentOfThread = new int[5];
+
+            for (int i = 0; i <= 4; i++)
+            {
+                this.PercentOfThread[i] = 100;
+            }
+            this.stationdaybox = new int[16];
+            for (int i = 0; i <= 15; i++)
+            {
+                this.stationdaybox[i] =0;
+            }
+        }
+        public int Get_Percent(int Task)
+        {
+            return this.PercentOfThread[Task];
+        }
+        public int updateData(int Case,DateTime dt,int stationID)
+        { 
+            string SQL;
+            switch (Case)
+            {
+                case 1://年
+                    {
+                        {
+                            DataSet ds;
+                            string sttime = dt.ToString("yy");
+                            string strSQL = "DECLARE	@return_value int EXEC	@return_value = [rfidtest].[GetCenterYearCount] @year = N'" + sttime + "',@station = 0";
+                            string strTable = " [db_rfidtest].[rfidtest].[dbo.goods]";
+                        
+                            try
+                            {
+                                InfoViewCommon.Classes.BaseOperate boperate = new InfoViewCommon.Classes.BaseOperate();
+                                ds = boperate.getds(strSQL, strTable);
+                            }
+                            catch
+                            {
+                                MessageBox.Show("网络连接失败！请稍后重试（错误 10273）");
+                                return 1;
+                            }
+                            try
+                            {
+
+                                for (int i = 0; i <= 11; i++)
+                                {
+                                    this.year[i] = int.Parse(ds.Tables[0].Rows[i]["sumbox"].ToString());
+                                }
+
+                            }
+                            catch (Exception xx)
+                            {
+                                MessageBox.Show("网络连接失败！请稍后重试（错误1027）" + xx.ToString());
+                                return 1;
+                            }
+
+                            return 0;
+                        }
+                        break;
+                    }
+                case 2://去年
+                    {
+                        {
+                            DataSet ds;
+
+                            string sttime = dt.AddMonths(-12).ToString("yy");
+                            string strSQL = "DECLARE	@return_value int EXEC	@return_value = [rfidtest].[GetCenterYearCount] @year = N'" + sttime + "',@station = 0";
+                            string strTable = " [db_rfidtest].[rfidtest].[dbo.goods]";
+                           // MessageBox.Show(strSQL);
+                            try
+                            {
+                                InfoViewCommon.Classes.BaseOperate boperate = new InfoViewCommon.Classes.BaseOperate();
+                                ds = boperate.getds(strSQL, strTable);
+                            }
+                            catch
+                            {
+                                MessageBox.Show("网络连接失败！请稍后重试（错误 10273）");
+                                return 1;
+                            }
+                            try
+                            {
+
+                                for (int i = 0; i <= 11; i++)
+                                {
+                                    this.lastyear[i] = int.Parse(ds.Tables[0].Rows[i]["sumbox"].ToString());
+                                }
+
+                            }
+                            catch (Exception xx)
+                            {
+                                MessageBox.Show("网络连接失败！请稍后重试（错误1027）" + xx.ToString());
+                                return 1;
+                            }
+
+                            return 0;
+                        }
+                        break;
+                    }
+                case 3://月
+                    {
+                        DataSet ds;
+                        string sttime = dt.ToString("yy-MM");
+                        string strSQL = "DECLARE	@return_value int EXEC	@return_value = [rfidtest].[GetCenterMonthCount] @Month = N'" + sttime + "',@numofday="+InfoViewCommon.Classes.Datetimecalc.daysofmonth(dt).ToString()+",@station = 0";
+                        string strTable = " [db_rfidtest].[rfidtest].[dbo.goods]";
+                        // MessageBox.Show(strSQL);
+                        try
+                        {
+                            InfoViewCommon.Classes.BaseOperate boperate = new InfoViewCommon.Classes.BaseOperate();
+                            ds = boperate.getds(strSQL, strTable);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("网络连接失败！请稍后重试（错误 10273）");
+                            return 1;
+                        }
+                        try
+                        {
+
+                            for (int i = 0; i <InfoViewCommon.Classes.Datetimecalc.daysofmonth(dt); i++)
+                            {
+                                this.month[i] = int.Parse(ds.Tables[0].Rows[i]["sumbox"].ToString());
+                            }
+
+                        }
+                        catch (Exception xx)
+                        {
+                            MessageBox.Show("网络连接失败！请稍后重试（错误10271）" + xx.ToString());
+                            return 1;
+                        }
+
+                        return 0;
+                        break;
+                    }
+                case 4://上月
+                    {
+                        DataSet ds;
+                        string sttime = dt.AddMonths(-1).ToString("yy-MM");
+                        string strSQL = "DECLARE	@return_value int EXEC	@return_value = [rfidtest].[GetCenterMonthCount] @Month = N'" + sttime + "',@numofday=" + InfoViewCommon.Classes.Datetimecalc.daysofmonth(dt).ToString() + ",@station = 0";
+                        string strTable = " [db_rfidtest].[rfidtest].[dbo.goods]";
+                        // MessageBox.Show(strSQL);
+                        try
+                        {
+                            InfoViewCommon.Classes.BaseOperate boperate = new InfoViewCommon.Classes.BaseOperate();
+                            ds = boperate.getds(strSQL, strTable);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("网络连接失败！请稍后重试（错误 10273）");
+                            return 1;
+                        }
+                        try
+                        {
+
+                            for (int i = 0; i < InfoViewCommon.Classes.Datetimecalc.daysofmonth(dt); i++)
+                            {
+                                this.lastmonth[i] = int.Parse(ds.Tables[0].Rows[i]["sumbox"].ToString());
+                            }
+
+                        }
+                        catch (Exception xx)
+                        {
+                            MessageBox.Show("网络连接失败！请稍后重试（错误10271）" + xx.ToString());
+                            return 1;
+                        }
+
+                        return 0;
+                        break; ;
+                    }
+                case 6://上12月
+                    {
+                        DataSet ds;
+                        string sttime = dt.AddMonths(-12).ToString("yy-MM");
+                        string strSQL = "DECLARE	@return_value int EXEC	@return_value = [rfidtest].[GetCenterMonthCount] @Month = N'" + sttime + "',@numofday=" + InfoViewCommon.Classes.Datetimecalc.daysofmonth(dt).ToString() + ",@station = 0";
+                        string strTable = " [db_rfidtest].[rfidtest].[dbo.goods]";
+                        // MessageBox.Show(strSQL);
+                        try
+                        {
+                            InfoViewCommon.Classes.BaseOperate boperate = new InfoViewCommon.Classes.BaseOperate();
+                            ds = boperate.getds(strSQL, strTable);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("网络连接失败！请稍后重试（错误 10273）");
+                            return 1;
+                        }
+                        try
+                        {
+
+                            for (int i = 0; i < InfoViewCommon.Classes.Datetimecalc.daysofmonth(dt); i++)
+                            {
+                                this.lastmonth[i] = int.Parse(ds.Tables[0].Rows[i]["sumbox"].ToString());
+                            }
+
+                        }
+                        catch (Exception xx)
+                        {
+                            MessageBox.Show("网络连接失败！请稍后重试（错误10271）" + xx.ToString());
+                            return 1;
+                        }
+
+                        return 0;
+                        break;
+                    }
+                case 5://站状态
+                    {
+                       
+                        {
+                            DataSet ds;
+                            string sttime = dt.ToString("yy-MM-dd");
+                            string strSQL = "DECLARE	@return_value int EXEC	@return_value = [rfidtest].[GetStationDetailbyDay] @day = N'" + sttime + "'";
+                            string strTable = " [db_rfidtest].[rfidtest].[dbo.goods]";
+                           // MessageBox.Show(strSQL);
+                            try
+                            {
+                                InfoViewCommon.Classes.BaseOperate boperate = new InfoViewCommon.Classes.BaseOperate();
+                                ds = boperate.getds(strSQL, strTable);
+                            }
+                            catch
+                            {
+                                MessageBox.Show("网络连接失败！请稍后重试（错误 10273）");
+                                return 1;
+                            }
+                            try
+                            {
+
+                                for (int i = 0; i <= 15; i++)
+                                {
+                                    this.stationdaybox[i] = int.Parse(ds.Tables[0].Rows[i]["sumbox"].ToString() );
+                                }
+
+                            }
+                            catch (Exception xx)
+                            {
+                                MessageBox.Show("网络连接失败！请稍后重试（错误1027）" + xx.ToString());
+                                return 1;
+                            }
+
+                            return 0;
+                        }
+                        break;
+                    }
+                default:
+                    {
+                        return 2;
+                        break;
+                    }
+                  
+            }
+            return 0;
+        }
+        
+
+    }
+}
---
+++  duanzheng2012/InfoViewTime/InfoViewTimeMain.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InfoViewTime
+{
+    public partial class InfoViewTimeMain : Form
+    {
+
+        private InfoViewCommon.Classes.ChartData chartdata = new InfoViewCommon.Classes.ChartData();
+
+        public InfoViewTimeMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InfoViewCommon.Classes.visifire vschart = new InfoViewCommon.Classes.visifire();
+            string str = System.AppDomain.CurrentDomain.BaseDirectory;
+            Uri url = new Uri(str + "chart/Demo.htm");
+            webBrowser.Url = url;
+
+            chartdata.updateData(5, DateTime.Now, 0).ToString();
+            vschart.reSize(webBrowser.Size.Width, webBrowser.Size.Height);
+            vschart.settitle("当日转运中心报表", "时间", "运输量");
+            string[] column = new string[16];
+            double[] data = new double[16];
+            for (int i = 0; i <= 15; i++)
+            {
+                column[i] = (i + 5).ToString() + ":00";
+                data[i] = chartdata.stationdaybox[i];
+            }
+
+            vschart.set3D(true);
+
+            Random rd = new Random();
+            vschart.setData(column, data, 16);
+            string type = "pie";
+
+            vschart.setType(type);
+            webBrowser.Url = vschart.displayChart();
+        }
+
+        private void InfoViewTimeMain_Resize(object sender, EventArgs e)
+        {
+        }
+    }
+}
